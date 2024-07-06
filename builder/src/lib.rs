951760fe859eaa7525c09ec5e436a088ebb93b41
xgh0627 <417644978@qq.com>
2024-07-06 23:12:04 +0800

    commit some basic code

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,50 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput, Ident, Type, Field};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident.to_string();
+    println!("结构体名称:{:?}", name);
     unimplemented!()
+}
+
+struct Fd {
+    name: Ident,
+    ty: Type,
+    optional: bool,
+}
+
+struct BuilderContext {
+    name: Ident,
+    fileds: Vec<Fd>,
+}
+
+impl From<Field> for Fd {
+    fn from(value: Field) -> Self {
+        let (optional,ty) = get_option_inner(&value.ty);
+        Self {
+            name: value.ident.unwrap(),
+            ty: ty.to_owned(),
+            optional: optional
+        }
+    }
+
+
+}fn get_option_inner(ty: &Type) -> (bool, &Type) {
+    if let Type::Path(syn::TypePath { path: syn::Path { segments, .. }, .. }) = ty {
+        if let Some(v) = segments.iter().next() {
+            if(v.ident == "Option") {
+                let t = match &v.arguments {
+                    syn::PathArguments::AngleBracketed(a) => match a.args.iter().next() {
+                        Some(syn::GenericArgument::Type(t)) => t,
+                        _ => panic!("Not sure what to do wth other GenericArgument")
+                    }
+                    _ => panic!("Not sure what to do with other PathArguments")
+                };
+                return (true,t);
+            }
+        }
+    }
+    return (false,ty)
 }
