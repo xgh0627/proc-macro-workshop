f504c73a3db11d19f377f468c2f7d0519fbbc2a5
许国辉 <417644978@qq.com>
2024-07-07 22:28:27 +0800

    fix builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,67 +1,163 @@
-use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput, Ident, Type, Field, Data};
-use syn::ext::IdentExt;
+use proc_macro2::{Ident, TokenStream};
+use quote::quote;
+use syn::{Data, DataStruct, DeriveInput, Field, Fields, FieldsNamed, GenericArgument, parse_macro_input, Path, Type, TypePath};
 
 #[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
-    let name = input.ident.to_string();
-    println!("结构体名称:{:?}", name);
-    unimplemented!()
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let dinput = parse_macro_input!(input as DeriveInput);
+    println!("{:?}",dinput);
+    BuilderContext::from(dinput).render().into()
 }
 
+/// 我们需要的描述一个字段的所有信息
 struct Fd {
     name: Ident,
     ty: Type,
     optional: bool,
 }
 
+/// 我们需要的描述一个 struct 的所有信息
 struct BuilderContext {
     name: Ident,
-    fileds: Vec<Fd>,
+    fields: Vec<Fd>,
 }
 
+/// 把一个 Field 转换成 Fd
 impl From<Field> for Fd {
-    fn from(value: Field) -> Self {
-        let (optional,ty) = get_option_inner(&value.ty);
+    fn from(f: Field) -> Self {
+        let (optional, ty) = get_option_inner(&f.ty);
         Self {
-            name: value.ident.unwrap(),
+            // 此时，我们拿到的是 NamedFields，所以 ident 必然存在
+            name: f.ident.unwrap(),
+            optional,
             ty: ty.to_owned(),
-            optional: optional
         }
     }
 }
 
+/// 把 DeriveInput 转换成 BuilderContext
 impl From<DeriveInput> for BuilderContext {
-    fn from(value: DeriveInput) -> Self {
-        let name = value.ident;
-        let fileds = if let Data::Struct(syn::DataStruct {fields:syn::Fields::Named(syn::FieldsNamed{named,..}),..}) = value.data {
+    fn from(input: DeriveInput) -> Self {
+        let name = input.ident;
+        let fields = if let Data::Struct(DataStruct {
+                                             fields: Fields::Named(FieldsNamed { named, .. }),
+                                             ..
+                                         }) = input.data
+        {
             named
-        }else{
-            panic!("Unsupported data type")
+        } else {
+            panic!("Unsupported data type");
         };
-        let fds = fileds.into_iter().map(Fd::from).collect();
-        Self {
-            name,
-            fileds:fds
-        }
+
+        let fds = fields.into_iter().map(Fd::from).collect();
+        Self { name, fields: fds }
     }
 }
 
-fn get_option_inner(ty: &Type) -> (bool, &Type) {
-    if let Type::Path(syn::TypePath { path: syn::Path { segments, .. }, .. }) = ty {
-        if let Some(v) = segments.iter().next() {
-            if(v.ident == "Option") {
-                let t = match &v.arguments {
-                    syn::PathArguments::AngleBracketed(a) => match a.args.iter().next() {
-                        Some(syn::GenericArgument::Type(t)) => t,
-                        _ => panic!("Not sure what to do wth other GenericArgument")
-                    }
-                    _ => panic!("Not sure what to do with other PathArguments")
-                };
-                return (true,t);
+impl BuilderContext {
+    pub fn render(&self) -> TokenStream {
+        let name = &self.name;
+        // 生成 XXXBuilder 的 ident
+        let builder_name = Ident::new(&format!("{}Builder", name), name.span());
+
+        let optionized_fields = self.gen_optionized_fields();
+        let methods = self.gen_methods();
+        let assigns = self.gen_assigns();
+
+        quote! {
+            /// Builder 结构
+            #[derive(Debug, Default)]
+            struct #builder_name {
+                #(#optionized_fields,)*
+            }
+
+            /// Builder 结构每个字段赋值的方法，以及 build() 方法
+            impl #builder_name {
+                #(#methods)*
+
+                pub fn build(mut self) -> Result<#name, &'static str> {
+                    Ok(#name {
+                        #(#assigns,)*
+                    })
+                }
+            }
+
+            /// 为使用 Builder 的原结构提供 builder() 方法，生成 Builder 结构
+            impl #name {
+                fn builder() -> #builder_name {
+                    Default::default()
+                }
             }
         }
     }
-    return (false,ty)
+
+    // 为 XXXBuilder 生成 Option<T> 字段
+    // 比如：executable: String -> executable: Option<String>
+    fn gen_optionized_fields(&self) -> Vec<TokenStream> {
+        self.fields
+            .iter()
+            .map(|Fd { name, ty, .. }| quote! { #name: std::option::Option<#ty> })
+            .collect()
+    }
+
+    // 为 XXXBuilder 生成处理函数
+    // 比如：methods: fn executable(mut self, v: impl Into<String>) -> Self { self.executable = Some(v); self }
+    fn gen_methods(&self) -> Vec<TokenStream> {
+        self.fields
+            .iter()
+            .map(|Fd { name, ty, .. }| {
+                quote! {
+                    pub fn #name(mut self, v: impl Into<#ty>) -> Self {
+                        self.#name = Some(v.into());
+                        self
+                    }
+                }
+            })
+            .collect()
+    }
+
+    // 为 XXXBuilder 生成相应的赋值语句，把 XXXBuilder 每个字段赋值给 XXX 的字段
+    // 比如：#field_name: self.#field_name.take().ok_or(" xxx need to be set!")
+    fn gen_assigns(&self) -> Vec<TokenStream> {
+        self.fields
+            .iter()
+            .map(|Fd { name, optional, .. }| {
+                if *optional {
+                    return quote! {
+                        #name: self.#name.take()
+                    };
+                }
+
+                quote! {
+                    #name: self.#name.take().ok_or(concat!(stringify!(#name), " needs to be set!"))?
+                }
+            })
+            .collect()
+    }
+}
+
+// 如果是 T = Option<Inner>，返回 (true, Inner)；否则返回 (false, T)
+fn get_option_inner(ty: &Type) -> (bool, &Type) {
+    // 首先模式匹配出 segments
+    if let Type::Path(TypePath {
+                          path: Path { segments, .. },
+                          ..
+                      }) = ty
+    {
+        if let Some(v) = segments.iter().next() {
+            if v.ident == "Option" {
+                // 如果 PathSegment 第一个是 Option，那么它内部应该是 AngleBracketed，比如 <T>
+                // 获取其第一个值，如果是 GenericArgument::Type，则返回
+                let t = match &v.arguments {
+                    syn::PathArguments::AngleBracketed(a) => match a.args.iter().next() {
+                        Some(GenericArgument::Type(t)) => t,
+                        _ => panic!("Not sure what to do with other GenericArgument"),
+                    },
+                    _ => panic!("Not sure what to do with other PathArguments"),
+                };
+                return (true, t);
+            }
+        }
+    }
+    (false, ty)
 }
